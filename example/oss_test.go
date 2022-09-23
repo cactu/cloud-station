c6674fae19a3b8eae9546d13e498b02c84cd1800
yuxy <yuxy@2345.com>
2022-09-23 15:19:00 +0800

    测试阿里云SDK

---
+++ example/oss_test.go
@@ -0,0 +1,59 @@
+package example
+
+import (
+	"fmt"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/joho/godotenv"
+	"os"
+	"testing"
+)
+
+var client *oss.Client
+
+var (
+	AccessKey    string
+	AccessSecret string
+	OssEndPoint  string
+	BucketName   string
+)
+
+func TestListBucket(t *testing.T) {
+	lsRes, err := client.ListBuckets()
+	if err != nil {
+		t.Log(err)
+	}
+
+	for _, bucket := range lsRes.Buckets {
+		fmt.Println("Buckets:", bucket.Name)
+	}
+}
+
+func TestUploadFile(t *testing.T) {
+	bucket, err := client.Bucket("my-bucket")
+	if err != nil {
+		t.Log(err)
+	}
+
+	err = bucket.PutObjectFromFile("my-object", "LocalFile")
+	if err != nil {
+		t.Log(err)
+	}
+}
+
+func init() {
+	//初始化配置文件
+	err := godotenv.Load("../etc/test.env")
+	if err != nil {
+		panic(err)
+	}
+	AccessKey = os.Getenv("ALI_AK")
+	AccessSecret = os.Getenv("ALI_SK")
+	OssEndPoint = os.Getenv("ALI_OSS_ENDPOINT")
+	BucketName = os.Getenv("ALI_BUCKET_NAME")
+	//初始化oss链接
+	c, err := oss.New(OssEndPoint, AccessKey, AccessSecret)
+	if err != nil {
+		panic(err)
+	}
+	client = c
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/cactu/cloud-station
+
+go 1.18
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v2.2.5+incompatible // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+)
