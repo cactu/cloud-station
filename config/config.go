a16e4f1bb2efcb243350d4530df8d62bad2ea41a
yuxy <yuxy@2345.com>
2022-09-23 18:55:44 +0800

    打印帮助信息

--- config/config.go
+++ config/config.go
@@ -1,50 +1,35 @@
-package example
+package config
 
 import (
 	"fmt"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/joho/godotenv"
 	"os"
-	"testing"
 )
 
-var client *oss.Client
-
 var (
+	Client       *oss.Client
 	AccessKey    string
 	AccessSecret string
 	OssEndPoint  string
 	BucketName   string
 )
 
-func TestListBucket(t *testing.T) {
-	lsRes, err := client.ListBuckets()
+func GetClientBucket() (*oss.Bucket, error) {
+	bucket, err := Client.Bucket(BucketName)
 	if err != nil {
-		t.Log(err)
+		fmt.Printf("获取bucket异常：%s\n", err.Error())
+		return nil, err
 	}
-
-	for _, bucket := range lsRes.Buckets {
-		fmt.Println("Buckets:", bucket.Name)
-	}
-}
-
-func TestUploadFile(t *testing.T) {
-	bucket, err := client.Bucket(BucketName)
-	if err != nil {
-		t.Log(err)
-	}
-
-	err = bucket.PutObjectFromFile("oss_test.go", "oss_test.go")
-	if err != nil {
-		t.Log(err)
-	}
+	return bucket, nil
 }
 
 func init() {
 	//初始化配置文件
-	err := godotenv.Load("../etc/test.env")
+	err := godotenv.Load("./etc/test.env")
 	if err != nil {
-		panic(err)
+		fmt.Printf("获取配置文件错误：%s\n", err)
+		os.Exit(1)
 	}
 	AccessKey = os.Getenv("ALI_AK")
 	AccessSecret = os.Getenv("ALI_SK")
@@ -53,7 +38,8 @@ func init() {
 	//初始化oss链接
 	c, err := oss.New(OssEndPoint, AccessKey, AccessSecret)
 	if err != nil {
-		panic(err)
+		fmt.Printf("初始化阿里云OSS错误：%s\n", err)
+		os.Exit(1)
 	}
-	client = c
+	Client = c
 }
